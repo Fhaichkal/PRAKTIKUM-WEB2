cfb4ef5df7d22cf81df57bf71f6d3b20c388d5b3
T Fikri Haikal <92281765+Fhaichkal@users.noreply.github.com>
2022-03-17 22:35:57 +0700

    Add files via upload

---
+++ Praktikum3web2/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once 'atas.php';
+?>          
+<div id="layoutSidenav_content">
+                <main>
+                    <h1>Hello guys wellcome to my website tes php 3</h1>  
+                </main>        
+<?php
+include_once 'bawah.php';
+?>
